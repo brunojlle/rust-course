aaca00dc6c71f488607a9876dbd753f44571c233
Bruno Ribeiro <bruno.ribsouza@gmail.com>
2025-01-15 17:25:19 -0300

    Loop

--- main.rs
+++ main.rs
@@ -47,6 +47,7 @@ fn main() {
     println!("Soma: {}", soma(2, 2));
 
     condicionais();
+    repeticoes();
 }
 
 fn condicionais() {
@@ -64,4 +65,29 @@ fn condicionais() {
 
     let condicao = if eh_maior { "maior" } else { "menor" };
     println!("É {} de idade", condicao);
+}
+
+fn repeticoes() {
+    let multiplicador:u8 = 5;
+
+    let mut contador:u8 = 0;
+    while contador < 10 {
+        contador += 1;
+
+        if contador == 5 {
+            continue;
+        }
+
+        println!("{} x {} = {}", multiplicador, contador, multiplicador * contador);
+    }
+
+    contador = 0;
+    loop {
+        contador += 1;
+        println!("{} x {} = {}", multiplicador, contador, multiplicador * contador);
+
+        if contador == 10 {
+            break;
+        }
+    }
 }
